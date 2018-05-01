cd3706afc3c722555ae58ef447cbbaa4b61ca8ee
Maksym Skorupskyi <33811372+MaksymSkorupskyi@users.noreply.github.com>
2018-05-01 11:49:58 +0300

    Add files via upload

---
+++ 30 Days of Code/Tuples.py
@@ -0,0 +1,24 @@
+"""Tuples
+
+Given an integer, n, and n space-separated integers as input, create a tuple, t, of those n integers.
+Then compute and print the result of hash(t).
+
+Note: hash(t) is one of the functions in the __builtins__ module, so it need not be imported.
+
+Input Format
+The first line contains an integer, , denoting the number of elements in the tuple.
+The second line contains  space-separated integers describing the elements in tuple .
+
+Output Format
+Print the result of hash(t).
+
+Sample Input 0
+2
+1 2
+
+Sample Output 0
+3713081631934410656
+"""
+
+n = int(input())
+print(hash(tuple(map(int, input().split()))))
