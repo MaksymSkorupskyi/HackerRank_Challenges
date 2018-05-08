b467c221bde2122ca9b3dcbcc5b8eeb3d6ea3665
Maksym Skorupskyi <33811372+MaksymSkorupskyi@users.noreply.github.com>
2018-05-08 11:34:03 +0300

    Add files via upload

--- 30 Days of Code/Day 04 - Class vs. Instance.py
+++ 30 Days of Code/Day 04 - Class vs. Instance.py
@@ -1,29 +1,29 @@
 """Class vs. Instance
 https://www.hackerrank.com/challenges/30-class-vs-instance/problem
 """
-class Person:
 
+
+class Person:
     def __init__(self, initialAge):
         # Add some more code to run some checks on initialAge
         if initialAge < 0:
-            age = 0
+            self.age = 0
             print('Age is not valid, setting age to 0.')
         else:
-            age = initialAge
+            self.age = initialAge
 
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
-        if age < 13:
+        if self.age < 13:
             print('You are young.')
-        elif age < 18:
+        elif self.age < 18:
             print('You are a teenager.')
         else:
             print('You are old.')
-    def yearPasses(self):
-        global age
-        # Increment the age of the person in here
-        age = age + 1
 
+    def yearPasses(self):
+        # Increment the age of the person in here
+        self.age += 1
 
 
 t = int(input())
