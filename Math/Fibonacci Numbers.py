edafafdf7cd57b21d71e1013b3bf4097b9e81800
Maksym Skorupskyi <33811372+MaksymSkorupskyi@users.noreply.github.com>
2018-05-29 15:19:57 +0300

    Add files via upload

--- Math/Fibonacci Numbers.py
+++ Math/Fibonacci Numbers.py
@@ -18,5 +18,26 @@ def fibonacci(n):
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
+# v3: recursion, optimized
+def fibonacci(n):
+    if n > 1:
+        return fibonacci(n - 1) + fibonacci(n -2)
+    return n
+
+# v4: recursion, optimized
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n -2)
+
+# generator
+# def fibonacci(n):
+#     a, b = 0, 1
+#     while a < n:
+#         yield a
+#         a, b = b, a + b
+
+
+
 n = int(input())
 print(fibonacci(n))
